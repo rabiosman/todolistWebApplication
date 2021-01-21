5b3580719ad3cfa96072c6f8d1c16e9c065d2dd7
rabiosman <rabiosman@gmail.com>
2021-01-20 23:24:52 -0300

    Adição de HTML, CSS e JS básicos para prosseguir com o teste da aplicação no Firebase

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+var authForm = document.getElementById('authForm')
+var authFormTitle = document.getElementById('authFormTitle')
+var register = document.getElementById('register')
+var access = document.getElementById('access')
+
+//Alterar o formulario de autenticacao para o cadastro de novas contas
+function toggleToRegister() {
+    authForm.submitAuthForm.innerHTML = 'Cadastrar conta'
+    authFormTitle.innerHTML = 'Insira seus dados para realizar o cadastro'
+
+    hideItem(register)
+    showItem(access)
+}
+
+//Alterar o formulario de autenticacao para o acesso de contas ja existentes
+function toggleToAccess() {
+    authForm.submitAuthForm.innerHTML = 'Acessar'
+    authFormTitle.innerHTML = 'Acesse a sua conta para continuar'
+
+    hideItem(access)
+    showItem(register)
+}
+
+//Simplifica a exibicao de elementos da pagina
+function showItem(element) {
+    element.style.display = 'block'
+}
+//Simplifica a remocao de elementos da pagina
+function hideItem(element) {
+    element.style.display = 'none'
+}
